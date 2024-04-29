a2384c066818f68f905151a6a0cad16816691b4f
NgJoeYi <s10262850@connect.np.edu.sg>
2024-04-29 15:28:11 +0800

    Question 1 Completed

---
+++ app/src/main/java/sg/edu/np/mad/madpractical3/MainActivity.java
@@ -0,0 +1,56 @@
+package sg.edu.np.mad.madpractical3;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.View;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        // Initialize a new User object
+        User user = new User( "John Doe", "MAD Developer", 1, false);
+
+        // Get the TextViews and Button from the layout
+        TextView tvName = findViewById(R.id.tvName);
+        TextView tvDescription = findViewById(R.id.tvDescription);
+        Button btnFollow = findViewById(R.id.btnFollow);
+
+        // Set the TextViews with the User's name, description and default button message
+        tvName.setText(user.name);
+        tvDescription.setText(user.description);
+        btnFollow.setText("Follow");
+
+        btnFollow.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                // Toggle the followed status
+                user.followed = !user.followed;
+
+                if (user.followed) {
+                    btnFollow.setText("Unfollow");
+                    Toast.makeText(getApplicationContext(), "Followed", Toast.LENGTH_SHORT).show();
+                } else {
+                    btnFollow.setText("Follow");
+                    Toast.makeText(getApplicationContext(),"Unfollowed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
