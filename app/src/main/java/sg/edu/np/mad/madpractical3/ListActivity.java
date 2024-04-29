acaf9cab9f738543571a656d370da75ba943d0aa
NgJoeYi <s10262850@connect.np.edu.sg>
2024-04-29 23:47:21 +0800

    Done with Practical 3

--- app/src/main/java/sg/edu/np/mad/madpractical3/ListActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical3/ListActivity.java
@@ -1,15 +1,19 @@
 package sg.edu.np.mad.madpractical3;
 
-import android.os.Bundle;
 import android.content.DialogInterface;
-import android.widget.ImageView;
-import androidx.activity.EdgeToEdge;
 import android.content.Intent;
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
 
 public class ListActivity extends AppCompatActivity {
 
@@ -24,29 +28,39 @@ public class ListActivity extends AppCompatActivity {
             return insets;
         });
 
-        // Creating AlertDialog.Builder inside onCreate method
-        AlertDialog.Builder builder = new AlertDialog.Builder(this);
-        builder.setTitle("Profile");
-        builder.setMessage("MADness");
-        builder.setCancelable(true);
+        ImageView profileImg = (ImageView) findViewById(R.id.imageView4);
 
-        builder.setPositiveButton("View", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                // Create an Intent to open the activity that displays activity_main.xml
-                Intent intent = new Intent(ListActivity.this, MainActivity.class);
-                startActivity(intent);
+        profileImg.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View V){
+                AlertDialog.Builder builder = new AlertDialog.Builder(ListActivity.this);
+
+                builder.setTitle("Profile");
+                builder.setMessage("MADness");
+                builder.setCancelable(true);
+                builder.setPositiveButton("View", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Random random = new Random();
+                        int randomNumber = random.nextInt(900000) + 100000;
+
+                        Intent activityName = new Intent(ListActivity.this, MainActivity.class);
+
+                        activityName.putExtra("randomNumber", randomNumber);
+                        startActivity(activityName);
+                        dialog.dismiss();
+                    }
+                });
+                builder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+
+                AlertDialog dialog = builder.create();
+
+                dialog.show();
             }
         });
-
-        builder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int id) {
-                // Action when "No" button is clicked
-                // Perform action here
-            }
-        });
-
-        // Create the AlertDialog
-        AlertDialog alertDialog = builder.create();
-        alertDialog.show();
     }
 }
--- app/src/main/java/sg/edu/np/mad/madpractical3/MainActivity.java
+++ app/src/main/java/sg/edu/np/mad/madpractical3/MainActivity.java
@@ -1,15 +1,17 @@
 package sg.edu.np.mad.madpractical3;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.view.View;
+import android.widget.Toast;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import android.widget.Toast;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         });
 
         // Initialize a new User object
-        User user = new User( "MAD", "MAD Developer", User.generateRandomId(), false);
+        User user = new User( "MAD", "MAD Developer", 1, false);
 
         // Get the TextViews and Button from the layout
         TextView tvName = findViewById(R.id.tvName);
@@ -33,25 +35,27 @@ public class MainActivity extends AppCompatActivity {
         Button btnFollow = findViewById(R.id.btnFollow);
 
         // Set the TextViews with the User's name, description and default button message
-        tvName.setText(user.name + " " + user.id);
+        tvName.setText(user.name);
         tvDescription.setText(user.description);
         btnFollow.setText("Follow");
 
         btnFollow.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
-                // Toggle the followed status
-                user.followed = !user.followed;
-
-                if (user.followed) {
+                if (user.followed){
                     btnFollow.setText("Unfollow");
+                    user.followed = false;
                     Toast.makeText(getApplicationContext(), "Followed", Toast.LENGTH_SHORT).show();
                 } else {
                     btnFollow.setText("Follow");
-                    Toast.makeText(getApplicationContext(),"Unfollowed", Toast.LENGTH_SHORT).show();
+                    user.followed = true;
+                    Toast.makeText(getApplicationContext(), "Unfollowed", Toast.LENGTH_SHORT).show();
                 }
-
             }
         });
+
+        Integer randomNumber = getIntent().getIntExtra("randomNumber", 0);
+        String name = user.name + " " + randomNumber;
+
+        tvName.setText(name);
     }
 }
